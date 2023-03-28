64d36228ae98b42dd213c3500820b00779796d7d
Felipe Baldim Guerra <fbaldim.guerra@gmail.com>
2023-03-28 17:26:08 -0300

    dist added and vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/ViteReactJS-API-Consume',
   plugins: [react()],
-  base: 'https://felipeguerra5.github.io/ViteReactJS-API-Consume/'
 })
