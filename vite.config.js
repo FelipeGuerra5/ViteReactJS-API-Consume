ca23f65eab569d1aefc526ab3f5d76ca65cebc8d
Felipe Baldim Guerra <fbaldim.guerra@gmail.com>
2023-03-28 17:20:39 -0300

    Updating to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ViteReactJS-API-Consume/'
+  base: 'https://felipeguerra5.github.io/ViteReactJS-API-Consume/'
 })
